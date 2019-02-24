82dfb90d2e151759367b5698443b9044c829ec00
anihakutin <4hbrody@gmail.com>
2019-02-24 03:37:32 -0500

    Allows users to create account, login and logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,34 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
+  enable :sessions
+  register Sinatra::Flash
+  register Sinatra::ActiveRecordExtension
+  set :session_secret, "youll_never_get_my_a**"
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
-  get "/" do
-    erb :index
+  get "/index" do
+    if logged_in?
+      @user = current_user
+      @setups = Setup.all
+      erb :index
+    else
+      redirect "/users/login"
+    end
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,59 @@
 class UsersController < ApplicationController
 
-  # GET: /users
-  get "/users" do
-    erb :"/users/index.html"
+  get "/users/signup" do
+    flash[:message_signup] = ""
+    erb :"/users/login.html"
   end
 
-  # GET: /users/new
-  get "/users/new" do
-    erb :"/users/new.html"
+  post "/signup" do
+    if params[:user][:username].empty? || params[:user][:email].empty? || params[:user][:password].empty?
+      flash[:message_signup] = "Fields Cannot Be Empty"
+      erb :"/users/login.html"
+    else
+      if User.find_by_email(params[:user][:email]) || User.find_by_username(params[:user][:username])
+        flash[:message_signup] = "Email Is Already Taken"
+        erb :"/users/login.html"
+      else
+        user = User.new(params[:user])
+        if user.save
+          session[:user_id] = user.id
+          redirect "/index"
+        else
+          flash[:message_signup] = "Signup Error, Please Try Again Later"
+          erb :"/users/login.html"
+        end
+      end
+    end
   end
 
-  # POST: /users
-  post "/users" do
-    redirect "/users"
+  get "/users/login" do
+    if logged_in?
+      redirect "/index"
+    else
+      erb :"/users/login.html"
+    end
   end
 
-  # GET: /users/5
-  get "/users/:id" do
-    erb :"/users/show.html"
+  post "/login" do
+    user = User.find_by_username(params[:user][:username])
+
+    if !user && user.authenticate(params[:user][:password])
+      flash[:message_login] = "Username or Password is incorrect"
+      erb :"/users/login.html"
+    else
+      session[:user_id] = user.id
+      redirect "/index"
+    end
+  end
+
+  get '/users/logout' do
+    session.clear
+    redirect "/users/login"
+  end
+
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :"/users/show"
   end
 
   # GET: /users/5/edit
@@ -34,4 +70,5 @@ class UsersController < ApplicationController
   delete "/users/:id/delete" do
     redirect "/users"
   end
+
 end
