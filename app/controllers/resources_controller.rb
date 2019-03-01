3d6c311a770d437b6ec41bcc1112efbb97c91aa6
anihakutin <4hbrody@gmail.com>
2019-03-01 16:04:39 -0500

    Remove extra redirects. Create New Resource page.

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -10,11 +10,38 @@ class ResourcesController < ApplicationController
 
   # GET: /resources/new
   get "/resources/new" do
+    @user = current_user
+
     erb :"/resources/new.html"
   end
 
   # POST: /resources
   post "/resources" do
+    if logged_in?
+      # Save images
+      filename = params[:file][:filename]
+      file = params[:file][:tempfile]
+
+      File.open("./public/images/#{filename}", 'wb') do |f|
+        f.write(file.read)
+      end
+
+      resource.create
+      # Update icon
+      resource.icon = "/images/#{filename}"
+      # Update Hardware
+      resource.name = params[:resource][:name]
+      resource.link = params[:resource][:name]
+      resource.rank = params[:resource][:rank]
+      resource.save
+
+      current_user.resources << resource
+
+      redirect "/resources/#{resource.id}"
+    else
+      flash[:message_edit] = "You must be logged in to create a resource"
+      redirect "/resources/#{params[:id]}"
+    end
     redirect "/resources"
   end
 
