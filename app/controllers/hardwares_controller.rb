01840383b6ef8484c9cf15e72b4a2d87445db991
anihakutin <4hbrody@gmail.com>
2019-02-28 19:59:13 -0500

    Hardware edit page update images. WIP.

--- app/controllers/hardwares_controller.rb
+++ app/controllers/hardwares_controller.rb
@@ -7,7 +7,7 @@ class HardwaresController < ApplicationController
 
     erb :"/hardwares/index.html"
   end
- 
+
   # GET: /hardwares/new
   get "/hardwares/new" do
     erb :"/hardwares/new.html"
@@ -28,12 +28,36 @@ class HardwaresController < ApplicationController
 
   # GET: /hardwares/5/edit
   get "/hardwares/:id/edit" do
-    erb :"/hardwares/edit.html"
+    # .hardwareable.is_a?(User)
+    @hardware = Hardware.find(params[:id])
+    if logged_in? && @hardware.hardwareable.id == current_user.id
+      @user = current_user
+      erb :"/hardwares/edit.html"
+    else
+      flash[:message_edit] = "Only a page owner can edit thier page"
+      redirect "/hardwares/#{params[:id]}"
+    end
   end
 
   # PATCH: /hardwares/5
   patch "/hardwares/:id" do
-    redirect "/hardwares/:id"
+
+    # Save image
+    @filename = params[:file][:filename]
+    file = params[:file][:tempfile]
+
+    File.open("./public/images/#{@filename}", 'wb') do |f|
+      f.write(file.read)
+    end
+
+    # Update Hardware
+    hardware = Hardware.find(params[:id])
+    hardware.name = params[:hardware][:name]
+    hardware.rank = params[:hardware][:rank]
+    hardware.image1 = "/images/#{@filename}"
+    hardware.save
+
+    redirect "/hardwares/#{hardware.id}"
   end
 
   # DELETE: /hardwares/5/delete
