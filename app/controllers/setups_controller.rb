9e3304e1d337f718104e2a099642743f4a4e44eb
anihakutin <4hbrody@gmail.com>
2019-03-03 21:05:49 -0500

    Setup edit controller create. Not tested

--- app/controllers/setups_controller.rb
+++ app/controllers/setups_controller.rb
@@ -34,13 +34,46 @@ class SetupsController < ApplicationController
   get "/setups/:id/edit" do
     @user = current_user
     @setup = Setup.find(params[:id])
-    
+
     erb :"/setups/edit.html"
   end
 
   # PATCH: /setups/5
   patch "/setups/:id" do
-    redirect "/setups/:id"
+    setup = Setup.find(params[:id])
+    if logged_in?
+      # Save images dynamically
+      filenames = []
+      files = []
+
+      if !params[:file].nil?
+        params[:file].each do |i|
+          filenames << i[1][:filename]
+          files << i[1][:tempfile]
+        end
+
+        files.each_with_index do |file, i|
+          File.open("./public/images/#{filenames[i]}", 'wb') do |f|
+            f.write(file.read)
+          end
+        end
+
+        # Update images dynamically
+        filenames.each_with_index do |filename, i|
+          setup.send("image#{i+1}=", "/images/#{filename}")
+        end
+      end
+
+      # Update Hardware
+      setup.name = params[:setup][:name]
+      setup.rank = params[:setup][:rank]
+      setup.save
+
+      redirect "/setups/#{setup.id}"
+    else
+      flash[:message_edit] = "Only a page owner can edit thier page"
+      redirect "/setups/#{params[:id]}"
+    end
   end
 
   # DELETE: /setups/5/delete
